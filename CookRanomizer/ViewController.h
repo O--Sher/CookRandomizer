6686cfad0dca716e39edf8ee00d79848e348419e
O_Sher <Skoolblako@gmail.com>
2015-11-11 22:01:13 +0200

    Initial Commit

---
+++ CookRanomizer/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  CookRanomizer
+//
+//  Created by O_Sher on 11.11.15.
+//  Copyright © 2015 OSher. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
